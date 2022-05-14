bebdf5510b348707ac51d91982dfea2aff75188f
delldu <18588220928@163.com>
2022-05-14 15:26:17 +0800

    Update

--- run_seq2.py
+++ run_seq2.py
@@ -1,29 +1,29 @@
 import argparse
 import os
-import sys
 from shutil import copyfile
 
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
-from models.models import DIFNet, DIFNet2
-from models.pwcNet import PwcNet
-from metrics import metrics
-from frame2vid import frame2vid
+from models.models import DIFNet2
+# from models.pwcNet import PwcNet
+# from metrics import metrics
+# from frame2vid import frame2vid
 
 from PIL import Image
 import numpy as np
-import math
 import pdb
-import time
-
+from tqdm import tqdm
 #python run_seq2.py --cuda --n_iter 3 --skip 2
+
+def load_tensor(filename):
+	image = Image.open(filename).convert("RGB")	
+	return torch.cuda.FloatTensor(np.array(image).transpose(2, 0, 1).astype(np.float32)[None,:,:,:] / 255.0)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model1', default='./trained_models/DIFNet2.pth') ####2
-parser.add_argument('--in_file', default='./data/Stab_te_reg/07/')
-parser.add_argument('--out_file', default='./output/OurStabReg2/07/')
+parser.add_argument('--in_file', default='images/')
+parser.add_argument('--out_file', default='output/')
 parser.add_argument('--n_iter', type=int, default=1, help='number of stabilization interations')
 parser.add_argument('--skip', type=int, default=1, help='number of frame skips for interpolation')
 #parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
@@ -53,58 +53,51 @@ DIFNet.eval()
 
 ##########################################################
 
-frameList = os.listdir(opt.in_file)
-frameList.sort()
+filename_list = os.listdir(opt.in_file)
+filename_list.sort()
+# filename_list[0:5] -- ['001.png', '002.png', '003.png', '004.png', '005.png']
 
-if os.path.exists(opt.out_file):
-	copyfile(opt.in_file + frameList[0], opt.out_file + frameList[0])
-	copyfile(opt.in_file + frameList[-1], opt.out_file + frameList[-1])
-else:
+
+if not os.path.exists(opt.out_file):
 	os.makedirs(opt.out_file)
-	copyfile(opt.in_file + frameList[0], opt.out_file + frameList[0])
-	copyfile(opt.in_file + frameList[-1], opt.out_file + frameList[-1])
-#end
+copyfile(opt.in_file + filename_list[0], opt.out_file + filename_list[0])
+copyfile(opt.in_file + filename_list[-1], opt.out_file + filename_list[-1])
 
 ### Generate output sequence
-for num_iter in range(opt.n_iter):
-	idx = 1
-	print('\nIter: ' + str(num_iter+1))
-	for f in frameList[1:-1]:
-		if f.endswith('.png'):
-			if num_iter == 0:
-				src = opt.in_file
-			else:
-				src = opt.out_file
-			#end
+destion = opt.out_file
+min_i = 0
+max_i = len(filename_list) - 1
+for n in range(opt.n_iter):
+	if n == 0:
+		source = opt.in_file
+	else:
+		source = opt.out_file
 
-			if idx < opt.skip or idx > (len(frameList)-1-opt.skip):
-				skip = 1
-			else:
-				skip = opt.skip
-			#end
+	for i in tqdm(range(1, max_i)):
+		if i - opt.skip >= 0:
+			fn_o_p = destion + filename_list[i - opt.skip]
+		else:
+			fn_o_p = destion + filename_list[min_i]
+		ft_o_p = load_tensor(fn_o_p)
 
-			fr_g1 = torch.cuda.FloatTensor(np.array(Image.open(opt.out_file + f[:-9] + '%05d.png' % (int(f[-9:-4])-skip))).transpose(2, 0, 1).astype(np.float32)[None,:,:,:] / 255.0)
-			#fr_g2 = torch.cuda.FloatTensor(np.array(Image.open(src + f)).transpose(2, 0, 1).astype(np.float32)[None,:,:,:] / 255.0)
-			fr_g3 = torch.cuda.FloatTensor(np.array(Image.open(src + f[:-9] + '%05d.png' % (int(f[-9:-4])+skip))).transpose(2, 0, 1).astype(np.float32)[None,:,:,:] / 255.0)
+		fn_i_c = source + filename_list[i]	
+		ft_i_c = load_tensor(fn_i_c)
 
-			#fr_o1 = torch.cuda.FloatTensor(np.array(Image.open(opt.in_file + f[:-9] + '%05d.png' % (int(f[-9:-4])-skip))).transpose(2, 0, 1).astype(np.float32)[None,:,:,:] / 255.0)
-			fr_o2 = torch.cuda.FloatTensor(np.array(Image.open(opt.in_file + f)).transpose(2, 0, 1).astype(np.float32)[None,:,:,:] / 255.0)
-			#fr_o3 = torch.cuda.FloatTensor(np.array(Image.open(opt.in_file + f[:-9] + '%05d.png' % (int(f[-9:-4])+skip))).transpose(2, 0, 1).astype(np.float32)[None,:,:,:] / 255.0)
+		if i + opt.skip < max_i:
+			fn_i_n = source + filename_list[i + opt.skip]
+		else:
+			fn_i_n = source + filename_list[max_i]
+		ft_i_n = load_tensor(fn_i_n)
 
-			with torch.no_grad():
-				fhat, I_int = DIFNet(fr_g1, fr_g3, fr_o2, fr_g3, fr_g1, 0.5) # Notice 0.5
+		with torch.no_grad():
+			fhat, I_int = DIFNet(ft_o_p, ft_i_n, ft_i_c, ft_i_n, ft_o_p, 0.5) # Notice 0.5
 
-			# Save image
-			#img = Image.fromarray(np.uint8(fhat.cpu().squeeze().permute(1,2,0)*255))
-			#img.save(opt.out_file + f)
+		# Save image
+		fn_o_c = destion + filename_list[i]
+		# print("Saving ", fn_o_p, fn_i_c, fn_i_n, "==>", fn_o_c, "...")
+		img = Image.fromarray(np.uint8(fhat.cpu().squeeze().permute(1,2,0)*255))
+		img.save(fn_o_c)
 
-			sys.stdout.write('\rFrame: ' + str(idx) + '/' + str(len(frameList)-2))
-			sys.stdout.flush()
-
-			idx += 1
-		#end
-	#end
-#end
 
 ### Make video
 #print('\nMaking video...')
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-python run_seq2.py --cuda --n_iter 3 --skip 2
+python run_seq2.py --cuda --n_iter 3 --skip 1
 
